3bdd0f6a9f9661cfc05e4252c15c91bdf35915b6
Berny Cantos <be@rny.cc>
2013-06-12 23:17:53 +0200

    fix checkout between branches pointing the same commit

--- git/hooks/post-checkout.d/show-description.sh
+++ git/hooks/post-checkout.d/show-description.sh
@@ -4,7 +4,7 @@ PREVIOUS_HEAD=$1
 NEW_HEAD=$2
 BRANCH_SWITCH=$3
 
-if [ $BRANCH_SWITCH == "1" -a $PREVIOUS_HEAD != $NEW_HEAD ]; then
+if [ $BRANCH_SWITCH == "1" ]; then
   DESCRIPTION=$(git config --get branch.$(git rev-parse --abbrev-ref HEAD).description 2> /dev/null)
   if [ "$DESCRIPTION" ]; then
     echo "Description: $DESCRIPTION"
