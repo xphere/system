f9cc2790a7e9bec647b5aafa435f909116a4875e
Berny Cantos <berny.cantos@ofertix.com>
2014-02-10 17:15:36 +0100

    [git] stash before linting

--- git/hooks/pre-commit.d/php-lint.sh
+++ git/hooks/pre-commit.d/php-lint.sh
@@ -7,8 +7,14 @@ if [ $(git diff $opts | wc -l) -eq 0 ]; then
     exit 0
 fi
 
-errors=()
+git stash save -u -k -- "PHP-Lint" >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo Error! Can\'t stash pending changes
+    exit 1
+fi
+
 files=()
+errors=()
 while read -r -d $'\0'; do
     files+=("$REPLY")
     error=$(php -l "$REPLY" -ddisplay_errors=1 -derror_reporting=E_ALL -dlog_errrors=0 2>&1 >/dev/null)
@@ -18,10 +24,15 @@ while read -r -d $'\0'; do
 done < <(git diff -z $opts)
 
 if [ ${#errors[@]} -ne 0 ]; then
-    echo KO!
-    echo ${errors[*]}
+    echo KO!\n${errors[*]}
     exit 1
 fi
 
 echo OK \(${#files[@]} files tested\)
+
+git cat-file -p stash@{0} | grep PHP-Lint >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    git stash pop >/dev/null 2>&1
+fi
+
 exit 0
