28c90d7beb91ff00ce628fe20de3cfe2647d3abf
Berny Cantos <berny.cantos@ofertix.com>
2014-02-10 17:15:06 +0100

    [git] skip early and extract parameters

--- git/hooks/pre-commit.d/php-lint.sh
+++ git/hooks/pre-commit.d/php-lint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 echo -n "PHP Lint — "
 
-skip=1
+opts='--cached --name-only --diff-filter=ACMR -- *.php'
+if [ $(git diff $opts | wc -l) -eq 0 ]; then
+    echo Skipping early… no PHP files found in the commit
+    exit 0
+fi
+
 errors=()
 files=()
 while read -r -d $'\0'; do
-    skip=0
     files+=("$REPLY")
     error=$(php -l "$REPLY" -ddisplay_errors=1 -derror_reporting=E_ALL -dlog_errrors=0 2>&1 >/dev/null)
     if [ -n "$error" ]; then
         errors+=($error);
     fi
-done < <(git diff -z --cached --name-only --diff-filter=ACMR -- '*.php')
-
-if [ $skip -eq 1 ]; then
-    echo Skipping… no PHP files found in the commit
-    exit 0
-fi
+done < <(git diff -z $opts)
 
 if [ ${#errors[@]} -ne 0 ]; then
     echo KO!
