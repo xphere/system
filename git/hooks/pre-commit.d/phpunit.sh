28f201bd195dc458676f04dd42f49884be2e6511
Berny Cantos <berny.cantos@ofertix.com>
2014-02-12 10:22:39 +0100

    skip if no phpunit.xml found

--- git/hooks/pre-commit.d/phpunit.sh
+++ git/hooks/pre-commit.d/phpunit.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 hook_title PHPUnit
 
-RESULT=$(phpunit -c app --stop-on-error --stop-on-failure)
+config=$(find -iname 'phpunit.xml*' | sort | head -1)
+if [ -z "$config" ]; then
+    hook_skip no PHPUnit configuration file found
+    exit 0
+fi
+
+RESULT=$(phpunit -c $(dirname "${config}") --stop-on-error --stop-on-failure)
 if [ $? -ne 0 ]; then
     hook_ko $(echo "${RESULT}"|grep -oP '(?<=^There was )[1-9][0-9]*+(?= failure)') failing tests
     echo "${RESULT}"
     exit 1
-else
-    hook_ok All tests passed
-    exit 0
 fi
+
+hook_ok All tests passed
